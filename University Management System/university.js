bb6494ff66d2bdb7c8a7695a36a43ceb2407ada6
Ramesh Das <157310459+Hydra-Ramesh@users.noreply.github.com>
2026-08-17 23:53:09 +0530

    Add University Routes

--- University Management System/university.js
+++ University Management System/university.js
@@ -1,8 +1,12 @@
 const Student = require('./students.js');
+const Teacher = require('./teacher.js');
+const Course = require('./course.js');
 class University{
     constructor(name){
         this.name = name;
         this.students = [];
+        this.teachers = [];
+        this.courses=[];
     }
 
     // Student Management
@@ -49,7 +53,7 @@ class University{
         student.department=newDepartment;
         console.log("Student department updated successfully!");
     }
-    studentByDepartment(department){
+    studentsByDepartment(department){
         const list=this.students.filter(s=>s.department===department);
         if(list.length===0){
             console.log("No students found in this department.");
@@ -62,29 +66,157 @@ class University{
 
     // Teacher Management
 
-    //1. Add teacher
-    //2. View all teachers
-    //3. Search teacher by employeeId
-    //4. Delete teacher by employeeId
+    addTeacher(teacher){
+        const exists = this.teachers.some(t=> t.employeeId===teacher.employeeId);
+        if(exists){
+            console.log("Teacher with this employee ID already exists.");
+            return false;
+        }
+        this.teachers.push(teacher);
+        console.log("Teacher added successfully!");
+        return true;
+    }
+    viewTeachers(){
+        if(this.teachers.length===0){
+            console.log("No teachers found.");
+            return;
+        }
+        this.teachers.forEach(teacher=>{
+            teacher.display();
+        })
+    }
+
+    findTeacher(employeeId){
+        return this.teachers.find(t=>t.employeeId===employeeId);
+    }
+    searchTeacher(employeeId){
+        const teacher=this.findTeacher(employeeId);
+        if(!teacher){
+            console.log("Teacher not found.");
+            return null;
+        }
+        teacher.display();
+        return teacher;
+    }
+    
+    deleteTeacher(employeeId){
+        const index = this.teachers.findIndex(teacher=> teacher.employeeId===employeeId);
+        if(index===-1){
+            console.log("Teacher not found");
+            return false;
+        }
+        const teacher = this.teachers[index];
+        if(teacher.courses.length>0){
+            console.log("Cannot delete teacher bcz Courses are assigned.")
+            return false;
+        }
+
+        this.teachers.splice(index,1);
+        console.log(`${teacher.name} deleted Successfully`);
+    }
 
 
 
     // Course Management
 
-    //1. Add course
-    //2. View all courses
-    //3. Search course by id
-    //4. Delete course by id
+    addCourse(course){
+        const exist = this.courses.some(c=>c.code === course.code);
+        if(exist){
+            console.log("Course already Exists");
+            return false;
+        }
+        this.courses.push(course);
+        console.log(`${course.name} added Successfully`);
+        return true;
+    }
+
+
+    viewCourses(){
+        if(this.courses.length===0){
+            console.log("No Courses Found.")
+            return;
+        }
+        console.log("\n==========COURSES===========\n");
+        this.courses.forEach(course=> {
+            course.display();
+        });
+    }
+
+
+    findCourse(code){
+        return this.courses.find(course=> course.code === code);
+    }
+    searchCourse(code){
+        const course = this.findCourse(code);
+        if(!course){
+            console.log("Course not found.");
+            return null;
+        }
+        course.display();
+        return course;
+    }
+
+
+    deleteCourse(code){
+        const index = this.courses.findIndex(course => course.code === code);
+        if(index===-1){
+            console.log("Course not Found.");
+            return false;
+        }
+        const course = this.courses[index];
+        if(course.student.length>0){
+            console.log("Cannot delete course because students are enrolled.");
+            return false;
+        }
+        course.removeTeacher();
+        this.courses.splice(index,1);
+        console.log(`${course.name} deleted succesfully.`);
+        return true;
+    }   
+
 
     // Course Operations
 
-    //1. Assign teacher to course
-    //2. Enroll student in course
-    //3. Remove student from course
-    //4. View enrolled students in course
+
+    assignTeacherToCourse(employeeId,courseCode){
+        const teacher = this.findTeacher(employeeId);
+        if(!teacher){
+            console.log("teacher not Found");
+            return false;
+        }
+        const course = this.findCourse(courseCode);
+        if(!course){
+            console.log("Course not Found");
+            return false;
+        }
+        return course.assignTeacher(teacher);
+    }
+
+
+    enrollStudent(roll,courseCode){
+        const student = this.students.find(s=> s.roll ===roll);
+        if(!student){
+            console.log("Student not Found");
+            return false;
+        }
+        const course = this.findCourse(courseCode);
+        if(!course){
+            console.log("Course not Found");
+            return false;
+        }
+        return course.enrollStudent(student);
+    }
 
 
 
+    removeStudentFromCourse(roll,courseCode){
+        const course = this.findCourse(courseCode);
+        if(!course){
+            console.log("Course not Found");
+            return false;
+        }
+        return course.removeStudent(roll);
+    }
 };
 
-
+module.exports = University;
